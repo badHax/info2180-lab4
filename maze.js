8e48b6dbf60ab71b9d9806191a5a8f519dfa1d95
badHax <odain.chevannes@gmail.com>
2016-10-13 21:10:12 -0500

    Answered quetion 4; The canvas can now be reset.

--- maze.js
+++ maze.js
@@ -1,21 +1,39 @@
 $(document).ready(function(){
 	var start_clicked = false;
+	var wall_touched = false;
 	//mouse touches boundary
 	$(".boundary").mouseover(function(){
-			$(".boundary").css({"background-color":"red"});//changed
+			if(start_clicked){
+				$(".boundary").css({"background-color":"red"});//changed
+				wall_touched = true;
+			}
+			
 		});
 	
 	//start button clicked
 	$("#start").click(function(){
 			$(this).css({"border":"4px solid blue"});
 			start_clicked = true;
+			
+			//reset the canvas
+			if(wall_touched){
+				restart();
+				}
 		});
 	
 	//end point successfully reached
 	$("#end").click(function(){
 			var boundary = $(".boundary").css("background-color");
-			 if(start_clicked && boundary != "red"){
-					alert(boundary);
+			 if(start_clicked && !wall_touched){
+					alert("You Win");
+					restart();
 				 }                                                                                                                                                                                                                                                                                                                                                                       
 		});
+		
+	function restart(){
+			$(".boundary").css({"background-color":"#eeeeee"});
+				$("#start").css({"border":"1px solid black"});
+				wall_touched = false;
+				start_clicked = false;
+		}
 });
