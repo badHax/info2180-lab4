33f04299152d0fc9404dfa78a249f04e150c9eed
badHax <odain.chevannes@gmail.com>
2016-10-13 20:00:30 -0500

    Answer to question 3; User notification added upon completion of the maze.

--- maze.js
+++ maze.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
-	
-	$(".boundary").mouseover(function red(){
+	var start_clicked = false;
+	//mouse touches boundary
+	$(".boundary").mouseover(function(){
 			$(".boundary").css({"background-color":"red"});//changed
+		});
+	
+	//start button clicked
+	$("#start").click(function(){
+			$(this).css({"border":"4px solid blue"});
+			start_clicked = true;
+		});
+	
+	//end point successfully reached
+	$("#end").click(function(){
+			var boundary = $(".boundary").css("background-color");
+			 if(start_clicked && boundary != "red"){
+					alert(boundary);
+				 }                                                                                                                                                                                                                                                                                                                                                                       
 		});
 });
