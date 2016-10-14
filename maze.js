404595784e25ce6f42f13445e170699f972ae9d3
badHax <odain.chevannes@gmail.com>
2016-10-13 21:41:58 -0500

    Answered question 5; Messages pop up in the actual html instead of alert boxes.

--- maze.js
+++ maze.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
 	var start_clicked = false;
 	var wall_touched = false;
+	var state_complete = false;
+	var dflt_msg = "Move your mouse over the 'S' to begin.";
+	
 	//mouse touches boundary
 	$(".boundary").mouseover(function(){
 			if(start_clicked){
 				$(".boundary").css({"background-color":"red"});//changed
 				wall_touched = true;
+				tell("--> YOU LOSE <--","red");
 			}
 			
 		});
 	
 	//start button clicked
 	$("#start").click(function(){
+			restart();
 			$(this).css({"border":"4px solid blue"});
 			start_clicked = true;
 			
@@ -25,7 +30,8 @@ $(document).ready(function(){
 	$("#end").click(function(){
 			var boundary = $(".boundary").css("background-color");
 			 if(start_clicked && !wall_touched){
-					alert("You Win");
+					tell("<-- YOU WIN!!! -->", "green");
+					state_complete = true;
 					restart();
 				 }                                                                                                                                                                                                                                                                                                                                                                       
 		});
@@ -35,5 +41,14 @@ $(document).ready(function(){
 				$("#start").css({"border":"1px solid black"});
 				wall_touched = false;
 				start_clicked = false;
+				if(state_complete){
+					state_complete = false;
+					return;
+					}
+				tell(dflt_msg,"black");
+		}
+	function tell(msg, col){
+			$("#status").html(msg);
+			$("#status").css({"color": col});
 		}
 });
